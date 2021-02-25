d2123f0f0793c8161fe5e73437ff5da850180ff7
Hamim Sazadah <hamim.sazadah@gmail.com>
2021-02-25 15:07:12 +0700

    Express Router

--- index.js
+++ index.js
@@ -16,30 +16,9 @@ app.use(express.static(__dirname + '/public'));
 //   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 // });
 app.use(bodyParser.json());
+const dishRouter = require('./routes/dishRouter');
 
-app.all('/dishes', (req,res,next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-});
-
-app.get('/dishes', (req,res,next) => {
-    res.end('Will send all the dishes to you!');
-});
-
-app.post('/dishes', (req, res, next) => {
-    console.log(req);
- res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.put('/dishes', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('PUT operation not supported on /dishes');
-});
- 
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all dishes');
-});
+app.use('/dishes', dishRouter);
 
 app.get('/dishes/:dishId', (req,res,next) => {
     res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
